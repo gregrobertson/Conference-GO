9f833e4e0bd6cb83676aab2198ac4596e5cc3803
Greg Robertson <grobertson743@gmail.com>
2022-06-07 16:20:59 -0700

    Cards/Rows/Shadows

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,51 @@
-window.addEventListener('DOMContentLoaded', () => {
+function createCard(name, description, pictureUrl) {
+    return `
+      <div>
+      <div class="shadow p-3 mb-5 bg-body rounded">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+        <div id="footer">${date}</div>
+        </div>
+      </div>
+    `;
+}
 
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/conferences/';
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            // Figure out what to do when the response is bad
+        } else {
+            const data = await response.json();
+
+            for (let conference of data.conferences) {
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    console.log(details)
+                    const name = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    const date = details.conference.starts
+                    console.log(date)
+                    const html = createCard(name, description, pictureUrl, date);
+
+                    const column = document.querySelector('.row');
+                    column.innerHTML += html;
+                }
+            }
+
+        }
+    } catch (e) {
+        console.error(e);
+    }
 
 });
