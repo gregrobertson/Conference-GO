a8c9ea743e970c3a2640e0091b9e88842a08907e
Greg Robertson <grobertson743@gmail.com>
2022-06-08 12:21:41 -0700

    Loaction.JS

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,1 +1,19 @@
 console.log(`Testing Shit Out Motha Fucka!`)
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = "http://localhost:8000/api/states/"
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+
+        const selectTag = document.getElementById('state')
+
+        for (let state of data.states) {
+            const option = document.createElement('option')
+        }
+    }
+})
