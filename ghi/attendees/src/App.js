5319946114fd171cf4358026377ad08098210296
Greg Robertson <grobertson743@gmail.com>
2022-06-10 13:12:23 -0700

    React/Bootstrap Done

--- ghi/attendees/src/App.js
+++ ghi/attendees/src/App.js
@@ -1,28 +1,34 @@
+import React from 'react';
+import Nav from './Nav';
+
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{attendee.name}</td>
-                <td>{attendee.conference}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <React.Fragment>
+      <Nav />
+      <div className="container-fluid">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Conference</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.attendees.map(attendee => {
+              return (
+                <tr key={attendee.href}>
+                  <td>{attendee.name}</td>
+                  <td>{attendee.conference}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </React.Fragment>
   );
 }
 
