c955eb25a5292fa32e51cee3c1fbe7d5313ee338
Greg Robertson <grobertson743@gmail.com>
2022-06-14 14:39:55 -0700

    SPA presentationFrom syntax errors

--- ghi/app/src/PresentationForm.js
+++ ghi/app/src/PresentationForm.js
@@ -99,56 +99,55 @@ class PresentationForm extends React.Component {
             console.log(data);
             this.setState({ conference: data.conference })
         }
+    }
 
-
-        render() {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="offset-3 col-6">
-                            <div className="shadow p-4 mt-4">
-                                <h1>Create a new presentation</h1>
-                                <form id="create-presentation-form">
-                                    <div className="form-floating mb-3">
-                                        <input placeholder="Presenter name" required type="text" name="presenter_name"
-                                            id="presenter_name" className="form-control" />
-                                        <label htmlFor="presenter_name">Presenter name</label>
-                                    </div>
-                                    <div className="form-floating mb-3">
-                                        <input placeholder="Presenter email" required type="email" name="presenter_email"
-                                            id="presenter_email" className="form-control" />
-                                        <label htmlFor="presenter_email">Presenter email</label>
-                                    </div>
-                                    <div className="form-floating mb-3">
-                                        <input placeholder="Company name" type="text" name="company_name" id="company_name"
-                                            className="form-control" />
-                                        <label htmlFor="company_name">Company name</label>
-                                    </div>
-                                    <div className="form-floating mb-3">
-                                        <input placeholder="Title" required type="text" name="title" id="title"
-                                            className="form-control" />
-                                        <label htmlFor="title">Title</label>
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="synopsis" className="form-label">Synopsis</label>
-                                        <textarea className="form-control" name="synopsis"
-                                            id="synopsis" rows="3"></textarea>
-                                    </div>
-                                    <div className="mb-3">
-                                        <select required name="conference" id="conference" className="form-select">
-                                            <option selected value="">Choose a conference</option>
-                                        </select>
-                                    </div>
-                                    <button className="btn btn-primary">Create</button>
-                                </form>
-                            </div>
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="offset-3 col-6">
+                        <div className="shadow p-4 mt-4">
+                            <h1>Create a new presentation</h1>
+                            <form id="create-presentation-form">
+                                <div className="form-floating mb-3">
+                                    <input placeholder="Presenter name" required type="text" name="presenter_name"
+                                        id="presenter_name" className="form-control" />
+                                    <label htmlFor="presenter_name">Presenter name</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input placeholder="Presenter email" required type="email" name="presenter_email"
+                                        id="presenter_email" className="form-control" />
+                                    <label htmlFor="presenter_email">Presenter email</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input placeholder="Company name" type="text" name="company_name" id="company_name"
+                                        className="form-control" />
+                                    <label htmlFor="company_name">Company name</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input placeholder="Title" required type="text" name="title" id="title"
+                                        className="form-control" />
+                                    <label htmlFor="title">Title</label>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="synopsis" className="form-label">Synopsis</label>
+                                    <textarea className="form-control" name="synopsis"
+                                        id="synopsis" rows="3"></textarea>
+                                </div>
+                                <div className="mb-3">
+                                    <select required name="conference" id="conference" className="form-select">
+                                        <option selected value="">Choose a conference</option>
+                                    </select>
+                                </div>
+                                <button className="btn btn-primary">Create</button>
+                            </form>
                         </div>
                     </div>
                 </div>
-            )
-        }
+            </div>
+        )
     }
+}
 
 
-
-export default PresentationForm
+export default PresentationForm;
